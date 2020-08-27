5e98bc02384a3fdf5ea2d3ec6397bebdbec117ee
AliceDTRH <57547425+AliceDTRH@users.noreply.github.com>
2020-08-27 19:48:34 +0200

    Fixed some issues comparing the processids

--- Alice's Window Closer App/Program.cs
+++ Alice's Window Closer App/Program.cs
@@ -6,22 +6,21 @@ using SHDocVw;
 using System.Threading;
 using System.Runtime.InteropServices;
 using Win32Interop.WinHandles;
-//Icon source: https://publicdomainvectors.org/en/free-clipart/White-cross-within-a-red-octagon-vector-image/17388.html
 
+//Icon source: https://publicdomainvectors.org/en/free-clipart/White-cross-within-a-red-octagon-vector-image/17388.html
 
 namespace Alice_s_Window_Closer_App
 {
-
     internal class Program
     {
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetConsoleWindow();
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        static extern int PostMessage(int hWnd, int msg, int wParam, int lParam);
+        private static extern int PostMessage(int hWnd, int msg, int wParam, int lParam);
 
         [DllImport("user32.dll", SetLastError = true)]
-        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
         private const int WM_Close = 16;
         private static bool force;
@@ -31,7 +30,7 @@ namespace Alice_s_Window_Closer_App
             //Resolves S1118:Utility classes should not have public constructors
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             if (args.Length == 1)
             {
@@ -40,6 +39,7 @@ namespace Alice_s_Window_Closer_App
                     case "--force":
                         force = true;
                         break;
+
                     default:
                         Console.Out.WriteLine("Alice's Window Closer App v1.0");
                         Console.Out.WriteLine("--force  Kill any processes that have an open window. (Dangerous!)");
@@ -59,24 +59,20 @@ namespace Alice_s_Window_Closer_App
 
             taskBarProcesses.ForEach((task) =>
             {
-
 #if DEBUG
                 if (task.ProcessName == "devenv")
                 {
                     return;
                 }
 #endif
-                Console.Out.WriteLine($"Closing {task.ProcessName}");
 
-                try
+                TopLevelWindowUtils.FindWindows((wh) => IsWindowHandleOwnedBy(wh, task)).ToList()
+                .ForEach((wh) =>
                 {
-                    TopLevelWindowUtils.FindWindows((wh) => IsWindowHandleOwnedBy(wh, task)).ToList()
-                    .ForEach((wh) => PostMessage(wh.RawPtr.ToInt32(), WM_Close, 0, 0));
-                }
-                catch (InvalidOperationException e)
-                {
-                    Console.Error.WriteLine($"Something went wrong trying to close {task.ProcessName}: {e.Message}");
-                }
+                    Console.Out.WriteLine($"Closing {task.ProcessName}");
+                    PostMessage(wh.RawPtr.ToInt32(), WM_Close, 0, 0);
+                });
+
                 if (force)
                 {
                     Thread.Sleep(500);
@@ -90,24 +86,17 @@ namespace Alice_s_Window_Closer_App
                     {
                         Console.Error.WriteLine($"Something went wrong trying to kill {task.ProcessName}: {e.Message}");
                     }
-
                 }
-
             });
 
-
-
+#if DEBUG
+            Console.ReadKey();
+#endif
         }
 
-        private static bool IsWindowHandleOwnedBy(WindowHandle wh, Process task)
-        {
-            if (!wh.IsValid || !wh.IsVisible()) { return false; }
-            GetWindowThreadProcessId(wh.RawPtr, out uint pid);
-            if (task.Id.Equals(pid))
-            {
-                return true;
-            }
-            return false;
-        }
+        private static bool IsWindowHandleOwnedBy(WindowHandle wh, Process task) => (
+            wh.IsValid && wh.IsVisible() &&
+            (GetWindowThreadProcessId(wh.RawPtr, out uint _) == GetWindowThreadProcessId(task.MainWindowHandle, out uint _))
+            );
     }
 }
