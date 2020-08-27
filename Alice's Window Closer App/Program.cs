e2b74b3ffe28718b9a6f0cf3463a9fb2e592f264
AliceDTRH <57547425+AliceDTRH@users.noreply.github.com>
2020-08-27 20:15:24 +0200

    Added feature to detect windows other than the main one (#3)
    * Added feature to detect windows other than the main one
    * Fixed help messages
    * Increased version number

--- Alice's Window Closer App/Program.cs
+++ Alice's Window Closer App/Program.cs
@@ -5,26 +5,35 @@ using System.Linq;
 using SHDocVw;
 using System.Threading;
 using System.Runtime.InteropServices;
+using Win32Interop.WinHandles;
+using System.Reflection;
 
 //Icon source: https://publicdomainvectors.org/en/free-clipart/White-cross-within-a-red-octagon-vector-image/17388.html
 
-
 namespace Alice_s_Window_Closer_App
 {
-
     internal class Program
     {
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetConsoleWindow();
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int PostMessage(int hWnd, int msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        private const int WM_Close = 16;
         private static bool force;
+
+        private static readonly Version version = Assembly.GetExecutingAssembly().GetName().Version;
 
         private Program()
         {
             //Resolves S1118:Utility classes should not have public constructors
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             if (args.Length == 1)
             {
@@ -33,10 +42,17 @@ namespace Alice_s_Window_Closer_App
                     case "--force":
                         force = true;
                         break;
+
+                    case "--version":
+                        Console.Out.WriteLine($"Alice's Window Closer App v{version}");
+                        Environment.Exit(0); //We close because we were only showing the version.
+                        break;
+
                     default:
-                        Console.Out.WriteLine("Alice's Window Closer App v1.0");
+                        Console.Out.WriteLine($"Alice's Window Closer App v{version.Major}.{version.MajorRevision}");
                         Console.Out.WriteLine("--force  Kill any processes that have an open window. (Dangerous!)");
                         Console.Out.WriteLine("--help   Show help flags");
+                        Environment.Exit(0); //We close because we were only showing the help.
                         break;
                 }
             }
@@ -52,42 +68,44 @@ namespace Alice_s_Window_Closer_App
 
             taskBarProcesses.ForEach((task) =>
             {
-
 #if DEBUG
                 if (task.ProcessName == "devenv")
                 {
                     return;
                 }
 #endif
-                Console.Out.WriteLine($"Closing {task.ProcessName}");
 
-                try
+                TopLevelWindowUtils.FindWindows((wh) => IsWindowHandleOwnedBy(wh, task)).ToList()
+                .ForEach((wh) =>
                 {
-                    task.CloseMainWindow();
-                }
-                catch (InvalidOperationException e)
-                {
-                    Console.Error.WriteLine($"Something went wrong trying to close {task.ProcessName}: {e.Message}");
-                }
+                    Console.Out.WriteLine($"Closing {task.ProcessName}");
+                    PostMessage(wh.RawPtr.ToInt32(), WM_Close, 0, 0);
+                });
+
                 if (force)
                 {
                     Thread.Sleep(500);
                     Console.Out.WriteLine($"Killing {task.ProcessName}");
                     try
                     {
-                        if (task.HasExited) { task.Kill(); }
+                        task.Refresh();
+                        if (!task.HasExited) { task.Kill(); }
                     }
                     catch (InvalidOperationException e)
                     {
                         Console.Error.WriteLine($"Something went wrong trying to kill {task.ProcessName}: {e.Message}");
                     }
-
                 }
-
             });
 
-
-
+#if DEBUG
+            Console.ReadKey();
+#endif
         }
+
+        private static bool IsWindowHandleOwnedBy(WindowHandle wh, Process task) => (
+            wh.IsValid && wh.IsVisible() &&
+            (GetWindowThreadProcessId(wh.RawPtr, out uint _) == GetWindowThreadProcessId(task.MainWindowHandle, out uint _))
+            );
     }
 }
